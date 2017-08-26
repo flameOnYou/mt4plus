85a7c130eddba734dee50cac28c2020fe477bd83
flameOnYou <songyu19@outlook.com>
2017-08-26 16:37:44 +0800

    打开历史数据窗口，接下来是锁定窗口

--- winUtils.py
+++ winUtils.py
@@ -86,6 +86,7 @@ def updateCharts():
     # 查找图表所有子窗口
     charts_list = []
     win32gui.EnumChildWindows(MDIClient, lambda hWnd, param: param.append(hWnd),  charts_list)
+    print "last:",charts_list
     for charts_hwnd in charts_list:
         if not charts_hwnd:  
             continue  
@@ -97,7 +98,28 @@ def updateCharts():
         
         if "Afx:" in clsname:
             win32api.PostMessage(charts_hwnd,WM_COMMAND,33324,0)
-            charts_hwnd = 0
+            
             print 'PostMessage :%s' % (title)
+
 if __name__ == '__main__':
-    updateCharts()
+    # updateCharts()
+    classname = "MetaQuotes::MetaTrader::4.00"
+    titlename = ""
+    #获取句柄
+    # 自顶层窗口（也就是桌面）开始搜索条件匹配的窗体，并返回这个窗体的句柄。不搜索子窗口、不区分大小写。找不到就返回0
+    mt4 = win32gui.FindWindow(classname, None)
+
+    WM_COMMAND = 0x0111
+    win32api.PostMessage(mt4, WM_COMMAND, 33053, 0 );
+
+    # import time
+    # time.sleep(1)
+    # # 历史数据对话框句柄
+    # historyTopic = "#32770 (对话框)"
+    # hst_titlename = "打开离线图表"
+    # hst_topic = win32gui.FindWindow(historyTopic, None)
+    # print hst_topic
+    # import ctypes
+    # user32_dll = ctypes.windll.LoadLibrary("user32.dll")
+    # user32_dll.GetDlgItem(mt4)
+    # hwnd2 = win32
